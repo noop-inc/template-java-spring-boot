379d9a5398c2504bc77bc428303e4587de3e7192
Chris Reidy <chris.reidy@rearc.io>
2023-12-08 12:29:56 -0500

    Adding in postgres config and entity classes.

--- src/test/java/dev/noop/blueprint/springboot/SpringbootApplicationTests.java
+++ src/test/java/dev/noop/blueprint/springboot/SpringbootApplicationTests.java
@@ -1,9 +1,17 @@
 package dev.noop.blueprint.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 class SpringbootApplicationTests {
 
 	@Test
